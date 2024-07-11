fd07a4d34639a458d45dd1464ff1c54b47636427
Wandana Mddumage <wandanamuthumali99@gmail.com>
2024-07-11 15:22:21 +0530

    feat: update admin dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,22 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoutes";
 import Admin from "./pages/Admin";
+import EditProduct from "./pages/EditProduct";
+import DeleteProduct from "./pages/DeleteProduct";
 
 function App() {
   return (
     <>
-      <Route
-        path="/admin/*"
-        element={
-          <ProtectedRoute>
-            <AdminRoutes />
-          </ProtectedRoute>
-        }
-      />
+      <Routes>
+        <Route
+          path="/admin/*"
+          element={
+            <ProtectedRoute>
+              <AdminRoutes />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </>
   );
 }
@@ -22,6 +26,8 @@ const AdminRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Admin />} />
+      <Route path="/product/edit/:id" element={<EditProduct />} />
+      <Route path="/product/delete/:id" element={<DeleteProduct />} />
     </Routes>
   );
 };
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,16 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: "./postcss.config.js",
-  },
+  plugins: [
+    react(),
+    {
+      ...tailwindcss("./tailwind.config.js"),
+      postcss: {
+        plugins: [autoprefixer],
+      },
+    },
+  ],
 });
